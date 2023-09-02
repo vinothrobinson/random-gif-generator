f001c1870f91c762d09b931d86b3cb1415a15ee4
vinothrobinson <vinothrobinson03@gmail.com>
2023-09-02 12:00:19 -0400

    Created a function to create new gifs, and added event listeners to the button and the form submission button

--- javascript.js
+++ javascript.js
@@ -12,3 +12,31 @@ function fetchGif(defaultString) {
             img.src = response.data.images.original.url;
         });
 }
+
+// Makes the form for changing the GIF appear and disappear
+let isHidden = true;
+document.querySelector(".new-gif").addEventListener("click", function() {
+    if (isHidden) {
+        const newGifForm = document.querySelector("#new-gif-form");
+        newGifForm.style.display = "flex";
+        isHidden = false;
+    } else {
+        const newGifForm = document.querySelector("#new-gif-form");
+        newGifForm.style.display = "none";
+        isHidden = true;
+    }
+})
+
+// Sends the entered GIF search to the function that changes the GIF displayed
+document.querySelector("#new-gif-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    createNewGif();
+})
+
+function createNewGif() {
+    const form = document.querySelector("#new-gif-form");
+    form.style.display = "none";
+    isHiddenP = true;
+    defaultString = document.querySelector("#title").value;
+    fetchGif(defaultString)
+}
